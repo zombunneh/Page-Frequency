387933a2d8e9a1bcdf45c409ec75a4b90fc3ec91
zombunneh <matthew.winterbourn@hotmail.co.uk>
2020-12-12 00:25:50 +0000

    Cleaning up main comments

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 # Access the page(s)
 # Scrape the text from it (relevant text only?)
 # Compile a frequency list of words from the page
-# Translate text from specified language to english (deepL? some library?)
+# Translate text from specified language to english
 # Display the results to the user
 # Export the results for use elsewhere e.g. anki deck (formatted in csv/txt etc)
+
+# Future improvements
+# TODO: add functionality to read a collection of webpages from a master page rather than having to add specific pages
 
 def print_hi(name):
     print(f'Hi, {name}')
 
+def main():
+    print('Enter in the URL to ')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
