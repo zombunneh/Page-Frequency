22f4780c81eefa3d6c825737d57dd95ffeffc454
zombunneh <matthew.winterbourn@hotmail.co.uk>
2020-12-12 00:19:04 +0000

    Started defining classes and methods for fetching webpages

---
+++ extraction/GetPages.py
@@ -0,0 +1,9 @@
+class GetPages:
+    x = 0
+
+# fetches a specified webpage
+    def fetch_page(self):
+        print("fetch_page called")
+
+    def fetch_multiple_pages(self):
+        print("fetch_multiple_pages called")
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# In here we call our methods to create the following workflow:
+# Query user to search through 1 page or multiple pages
+# Access the page(s)
+# Scrape the text from it (relevant text only?)
+# Compile a frequency list of words from the page
+# Translate text from specified language to english (deepL? some library?)
+# Display the results to the user
+# Export the results for use elsewhere e.g. anki deck (formatted in csv/txt etc)
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
