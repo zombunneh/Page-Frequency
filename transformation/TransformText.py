3b9c3f1bc165d664699a8119fc3f7e8a2a8a49c2
zombunneh <matthew.winterbourn@hotmail.co.uk>
2020-12-16 01:49:37 +0000

    def token_text_by_space in TransformText.py

--- transformation/TransformText.py
+++ transformation/TransformText.py
@@ -1,8 +1,14 @@
 import nltk
+import re
 
 
 # Class tokenizes the text given to it using regex
 class TransformText:
+    words = []
 
-    def tokenise_text(self, text):
-        text = ''
+    # Separates text by white space, and converts to lowercase
+    def tokenise_text_by_space(self, text):
+        ps = '\w+'
+        token_text = re.findall(ps, text)
+        for word in token_text:
+            self.words.append(word.lower())
